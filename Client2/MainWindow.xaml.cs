73235fbb5b10f9076e993502f88873b94f95a557
Antoine GIRARD <sapk@sapk.fr>
2016-12-16 13:49:55 +0100

    Init TP3

---
+++ Client2/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+
+namespace Client
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RemotingInterface.IRemotChaine LeRemot;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // l'objet LeRemot  récupére ici la référence de l'objet du serveur
+            // on donne l'URI (serveur, port, classe du serveur)  et le nom de l'interface
+            LeRemot = (RemotingInterface.IRemotChaine)Activator.GetObject(
+                typeof(RemotingInterface.IRemotChaine), "tcp://localhost:12345/Serveur");
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = LeRemot.Hello().ToString();
+        }
+    }
+}
---
+++ ConsoleApplication2/Serveur.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+
+namespace Serveur
+{
+    class Serveur : MarshalByRefObject, RemotingInterface.IRemotChaine
+    {
+        static void Main(string[] args)
+        {
+            // Création d'un nouveau canal pour le transfert des données via un port 
+            TcpChannel canal = new TcpChannel(12345);
+
+            // Le canal ainsi défini doit être Enregistré dans l'annuaire
+            ChannelServices.RegisterChannel(canal, false);
+
+            // Démarrage du serveur en écoute sur objet en mode Singleton
+            // Publication du type avec l'URI et son mode 
+            RemotingConfiguration.RegisterWellKnownServiceType(
+                typeof(Serveur), "Serveur", WellKnownObjectMode.Singleton);
+
+            Console.WriteLine("Le serveur est bien démarré");
+            // pour garder la main sur la console
+            Console.ReadLine();
+        }
+        public override object InitializeLifetimeService()
+        {
+            return null;
+        }
+        public string Hello()
+        {
+            // TODO : ajoutez l'implémentation de Serveur.Hello
+            return "la chaine se trouvant sur le serveur";
+        }
+
+    }
+}
